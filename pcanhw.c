487945e3fecdee87460d1ccbaf078f2529369e7d
Oliver Hartkopp <socketcan@hartkopp.net>
2016-06-18 16:30:19 +0200

    Added new hardware and flash types from latest PcanFlash.ini
    Signed-off-by: Oliver Hartkopp <socketcan@hartkopp.net>

--- pcanhw.c
+++ pcanhw.c
@@ -144,7 +144,83 @@ const fblock_t hw31[] = {
 	{0x3C000, 0x2000}
 };
 
+const fblock_t hw34[] = {
+	{0x2000, 0x2000},
+	{0x4000, 0x2000},
+	{0x6000, 0x2000},
+	{0x8000, 0x2000},
+	{0xA000, 0x2000},
+	{0xC000, 0x2000},
+	{0xE000, 0x2000},
+	{0x10000, 0x10000},
+	{0x20000, 0x10000},
+	{0x30000, 0x2000},
+	{0x32000, 0x2000},
+	{0x34000, 0x2000},
+	{0x36000, 0x2000},
+	{0x38000, 0x2000},
+	{0x3A000, 0x2000},
+	{0x3C000, 0x2000}
+};
+
 const fblock_t hw35[] = {
+	{0x2000, 0x2000},
+	{0x4000, 0x2000},
+	{0x6000, 0x2000},
+	{0x8000, 0x2000},
+	{0xA000, 0x2000},
+	{0xC000, 0x2000},
+	{0xE000, 0x2000},
+	{0x10000, 0x10000},
+	{0x20000, 0x10000},
+	{0x30000, 0x2000},
+	{0x32000, 0x2000},
+	{0x34000, 0x2000},
+	{0x36000, 0x2000},
+	{0x38000, 0x2000},
+	{0x3A000, 0x2000},
+	{0x3C000, 0x2000}
+};
+
+const fblock_t hw36[] = {
+	{0x2000, 0x2000},
+	{0x4000, 0x2000},
+	{0x6000, 0x2000},
+	{0x8000, 0x2000},
+	{0xA000, 0x2000},
+	{0xC000, 0x2000},
+	{0xE000, 0x2000},
+	{0x10000, 0x10000},
+	{0x20000, 0x10000},
+	{0x30000, 0x2000},
+	{0x32000, 0x2000},
+	{0x34000, 0x2000},
+	{0x36000, 0x2000},
+	{0x38000, 0x2000},
+	{0x3A000, 0x2000},
+	{0x3C000, 0x2000}
+};
+
+const fblock_t hw37[] = {
+	{0x2000, 0x2000},
+	{0x4000, 0x2000},
+	{0x6000, 0x2000},
+	{0x8000, 0x2000},
+	{0xA000, 0x2000},
+	{0xC000, 0x2000},
+	{0xE000, 0x2000},
+	{0x10000, 0x10000},
+	{0x20000, 0x10000},
+	{0x30000, 0x2000},
+	{0x32000, 0x2000},
+	{0x34000, 0x2000},
+	{0x36000, 0x2000},
+	{0x38000, 0x2000},
+	{0x3A000, 0x2000},
+	{0x3C000, 0x2000}
+};
+
+const fblock_t hw40[] = {
 	{0x2000, 0x2000},
 	{0x4000, 0x2000},
 	{0x6000, 0x2000},
@@ -178,8 +254,16 @@ uint32_t crc_startpos(uint8_t hw_type)
 		return 0x03DF00; /* PCAN-Router Pro */
 	case 31:
 		return 0x03DF00; /* PCAN-RS-232 */
+	case 34:
+		return 0x000100; /* PCAN-USB pro FD */
 	case 35:
 		return 0x03DF00; /* PCAN-Router DR */
+	case 36:
+		return 0x000100; /* PCAN-USB FD */
+	case 37:
+		return 0x000100; /* PCAN-GPS */
+	case 40:
+		return 0; /* PCAN-Router FD */
 	default:
 		return 0; /* disabled */
 	}
@@ -200,8 +284,16 @@ int get_hw_xor_flip(uint8_t hw_type)
 		return 1; /* PCAN-Router Pro */
 	case 31:
 		return 1; /* PCAN-RS-232 */
+	case 34:
+		return 1; /* PCAN-USB Pro FD */
 	case 35:
 		return 1; /* PCAN-Router DR */
+	case 36:
+		return 1; /* PCAN-USB FD */
+	case 37:
+		return 1; /* PCAN-GPS */
+	case 40:
+		return 1; /* PCAN-Router FD */
 	default:
 		return 1; /* enabled by default */
 	}
@@ -222,8 +314,16 @@ char *hw_name(uint8_t hw_type)
 		return "PCAN-Router Pro";
 	case 31:
 		return "PCAN-RS-232";
+	case 34:
+		return "PCAN-USB Pro FD";
 	case 35:
 		return "PCAN-Router DR";
+	case 36:
+		return "PCAN-USB FD";
+	case 37:
+		return "PCAN-GPS";
+	case 40:
+		return "PCAN-Router FD";
 	default:
 		return "unkown";
 	}
@@ -240,6 +340,14 @@ char *flash_name(uint8_t flash_type)
 		return "M32C87_1MB";
 	case 16:
 		return "LPC23xx_256k";
+	case 20:
+		return "USB_PRO_FD_Flash";
+	case 21:
+		return "USB_FD_Flash";
+	case 22:
+		return "LPC407X_8X";
+	case 40:
+		return "FLASH_ROUTER_FD";
 	default:
 		return "unkown";
 	}
@@ -276,10 +384,30 @@ int num_flashblocks(uint8_t hw_type)
 		/* PCAN-RS-232 */
 		return sizeof(hw31) / sizeof(hw31[0]);
 		break;
+#if 0
+	case 34:
+		/* PCAN-USB Pro FD */
+		return sizeof(hw34) / sizeof(hw34[0]);
+		break;
+#endif
 	case 35:
 		/* PCAN-Router DR */
 		return sizeof(hw35) / sizeof(hw35[0]);
 		break;
+#if 0
+	case 36:
+		/* PCAN-USB FD */
+		return sizeof(hw36) / sizeof(hw36[0]);
+		break;
+	case 37:
+		/* PCAN-GPS */
+		return sizeof(hw37) / sizeof(hw37[0]);
+		break;
+	case 40:
+		/* PCAN-Router FD */
+		return sizeof(hw40) / sizeof(hw40[0]);
+		break;
+#endif
 	default:
 		return 0;
 	}
@@ -322,10 +450,30 @@ void erase_flashblocks(int s, FILE *infile, uint8_t module_id, uint8_t hw_type, 
 		startaddr = hw31[index].start;
 		blksz = hw31[index].len;
 		break;
+	case 34:
+		/* PCAN-USB Pro FD */
+		startaddr = hw34[index].start;
+		blksz = hw34[index].len;
+		break;
 	case 35:
 		/* PCAN-Router DR */
 		startaddr = hw35[index].start;
 		blksz = hw35[index].len;
+		break;
+	case 36:
+		/* PCAN-USB FD */
+		startaddr = hw36[index].start;
+		blksz = hw36[index].len;
+		break;
+	case 37:
+		/* PCAN-GPS */
+		startaddr = hw37[index].start;
+		blksz = hw37[index].len;
+		break;
+	case 40:
+		/* PCAN-Router FD */
+		startaddr = hw40[index].start;
+		blksz = hw40[index].len;
 		break;
 	default:
 		fprintf(stderr, "bad flashblocks entry found for hardware type %d (%s)!\n",
